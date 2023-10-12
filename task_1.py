9dcaf80167c947729ada80bef9f0de0d96d50440
unknown <FrishStrike@mail.ru>
2023-10-12 16:14:14 +0300

    First task

---
+++ task_1.py
@@ -0,0 +1,8 @@
+# 1
+number = float(input("Введите число: "))
+sign = int(input("Введите количество знаков после запятой: "))
+print(round(number, sign))
+# 2
+first_number = float(input("Введите первое число: "))
+second_number = float(input("Введите второе число: "))
+print(int((first_number + second_number)/2))
